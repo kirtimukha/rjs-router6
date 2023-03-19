60408e9bd63860f518ebad6bdafaca0d1adc1001
stellamax <kirtimukha@nate.com>
2023-03-19 20:39:23 +0900

    [U] useNavigate

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -12,7 +12,7 @@ const Header = () => {
         <ul>
         <li><Link to={"/"}>Home</Link></li>
         {/*<li><Link to={"/about"}>About</Link></li>*/}
-        <li> <button onClick={onAboutClick}>About</button> </li>
+        <li onClick={onAboutClick}>About </li>
         </ul>
       </header>
     </>
